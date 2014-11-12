5aa93bc709d86e45ee4d466a303361c3c287f6b6
dam205 <danielsimon209@gmail.com>
2014-11-12 09:31:55 +0100

    Primitiva2

--- src/fernandowirtz/primitiva/Primitiva.java
+++ src/fernandowirtz/primitiva/Primitiva.java
@@ -6,15 +6,24 @@ public class Primitiva {
 
 	private int[] numeros = new int[6];
 	private String resultado = "";
+	boolean repetido;
 
 	public String calcular() {
 		Random aleatorio = new Random();
-		for (int f=0;f<numeros.length;f++) {
+		for (int f = 0; f < numeros.length; f++) {
+			repetido = false;
 			numeros[f] = aleatorio.nextInt(48) + 1;
-			resultado = resultado + (String.valueOf(numeros[f])+" ");
+			for (int i = 0; i < f; i++) {
+				if (numeros[f] == numeros[i]) {
+					f--;
+					repetido = true;
+				}
+			}
+			if (repetido == false) {
+				resultado = resultado + (String.valueOf(numeros[f]) + " ");
+			}
 		}
 		return resultado;
 	}
-	
 
 }
